4cf6376aea2ad3ec13b7da24d5bbcb340581e867
Geoffrey Cox <gcox3d@gmail.com>
2016-11-26 22:03:47 -0800

    added html in requestHandlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,13 +4,13 @@ let exec = require('child_process').exec;
 
 function start(res) {
   console.log('request handler start was called');
+  let body = '<html>' + '<head>' + '<meta http-equiv="Content-Type" content="text/html; ' + 'charset=UTF-8" />' + '</head>' + '<body>' + '<form action="/upload" method="post">' + '<textarea name="text" rows="20" cols="60"></textarea>' + '<input type="submit" value="Submit text" />' + '</form>' + '</body>' + '</html>';
   exec('ls -lah', function(error, stdout, stderr) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(stdout);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(body);
     res.end();
   });
 }
-
 function upload(res) {
   console.log('request handler upload was called');
   res.writeHead(200, {'Content-Type': 'text/plain'});
