f0349c82800abeda33cb520590923a46e5eaf2ba
Geoffrey Cox <gcox3d@gmail.com>
2016-11-26 18:00:59 -0800

    added url module. added console log for request path.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 'use strict';
 
 let http = require('http');
+let url = require('url');
 
 function start() {
   function onRequest(req, res) {
+    let pathName = url.parse(req.url).pathName;
+    console.log('request for ' + pathName + ' recieved');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World');
     res.end();
