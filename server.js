7581ef75c0fe4b2d087ac5fbc13c84e3b10a118f
Geoffrey Cox <gcox3d@gmail.com>
2016-11-26 17:45:06 -0800

    reworked server.js code, added console.log

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 
 let http = require('http');
 
-http.createServer(function(req, res) {
+function onRequest(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World');
   res.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+console.log('ther server has started');
