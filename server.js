220b62b3196285f9585527cb48eb5fcbe039637f
Geoffrey Cox <gcox3d@gmail.com>
2016-11-26 22:08:54 -0800

    add listeners to server

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ let url = require('url');
 
 function start(route, handle) {
   function onRequest(req, res) {
+    let postData = '';
     let pathname = url.parse(req.url).pathname;
     console.log('request for ' + pathname + ' recieved');
-    route(handle, pathname, res);
+    res.setEncoding('utf-8');
+    res.addListener ('data', function(postDataChunk) {
+      postData += postDataChunk;
+      console.log('recieved POST data chunk ' + postDataChunk);
+    });
+    res.addListener('end', function() {
+      route(handle, pathname, res, postData);
+    });
   }
   http.createServer(onRequest).listen(8888);
   console.log('ther server has started');
