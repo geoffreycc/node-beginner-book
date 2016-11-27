03ed9154d49d3006c0acbfdc370408fa0ff8394c
Geoffrey Cox <gcox3d@gmail.com>
2016-11-26 21:31:54 -0800

    added sleep function to requestHandlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,10 +2,18 @@
 
 function start() {
   console.log('request handler start was called');
+
+  function sleep(miliseconds) {
+    let startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + miliseconds);
+  }
+  sleep(10000);
+  return 'hello start';
 }
 
 function upload() {
   console.log('request handler upload was called');
+  return 'hello upload';
 }
 
 exports.start = start;
