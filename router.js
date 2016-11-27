406e158b29a11c2804f4c16493df8d51f3d9008d
Geoffrey Cox <gcox3d@gmail.com>
2016-11-26 21:04:41 -0800

    updated router and server files to use requestHandlers

--- router.js
+++ router.js
@@ -1,7 +1,12 @@
 'use strict';
 
-function route(pathname) {
+function route(handle, pathname) {
   console.log( 'about to route a request for ' + pathname);
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname]();
+  } else {
+    console.log('no request handler found for ' + pathname);
+  }
 }
 
 exports.route = route;
--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 let http = require('http');
 let url = require('url');
 
-function start(route) {
+function start(route, handle) {
   function onRequest(req, res) {
     let pathname = url.parse(req.url).pathname;
     console.log('request for ' + pathname + ' recieved');
-    route(pathname);
+    route(handle, pathname);
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World');
