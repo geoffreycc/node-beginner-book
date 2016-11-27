6fb9136dbff56e029063c408b77bb95853af39a2
Geoffrey Cox <gcox3d@gmail.com>
2016-11-26 22:59:59 -0800

    change body in requestHandlers.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,8 +4,7 @@ let fs = require('fs');
 
 function start(res, postData) {
   console.log('request handler start was called');
-  let body = '<html>' + '<head>' + '<meta http-equiv="Content-Type" content="text/html; ' + 'charset=UTF-8" />' + '</head>' + '<body>' + '<form action="/upload" method="post">' + '<textarea name="text" rows="20" cols="60"></textarea>' + '<input type="submit" value="Submit text" />' + '</form>' + '</body>' + '</html>';
-
+  let body = '<html>' + '<head>' + '<meta http-equiv="Content-Type" content="text/html; ' + 'charset=UTF-8" />' + '</head>' + '<body>' + '<form action="/upload" enctype="multipart/form-data" method="post">' + '<input type="file" name="upload">' + '<input type="submit" value="Upload File" />' + '</form>' + '</body>' + '</html>';
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(body);
   res.end();
