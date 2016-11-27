b8f3e0e35d6c9a55eab472f53755ba0262dea2b0
Geoffrey Cox <gcox3d@gmail.com>
2016-11-26 23:20:31 -0800

    updated requestHandlers.js to use formidable

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,34 @@
 'use strict';
-let querystring = require('querystring');
 let fs = require('fs');
+let formidable = require('formidable');
 
-function start(res, postData) {
+function start(res) {
   console.log('request handler start was called');
-  let body = '<html>' + '<head>' + '<meta http-equiv="Content-Type" content="text/html; ' + 'charset=UTF-8" />' + '</head>' + '<body>' + '<form action="/upload" enctype="multipart/form-data" method="post">' + '<input type="file" name="upload">' + '<input type="submit" value="Upload File" />' + '</form>' + '</body>' + '</html>';
+  let body = '<html>' + '<head>' + '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' + '</head>' + '<body>' + '<form action="/upload" enctype="multipart/form-data" method="post">' + '<input type="file" name="upload" multiple="multiple">' + '<input type="submit" value="Upload File" />' + '</form>' + '</body>' + '</html>';
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(body);
   res.end();
 }
-function upload(res, postData) {
+
+function upload(res, req) {
   console.log('request handler upload was called');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('you have sent ' + querystring.parse(postData).text);
-  res.end();
+  let form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(req, function(error, fields, files) {
+    console.log('parsing done');
+    fs.rename(files.upload.path, '/tmp/test.jpg', function(error) {
+      if(error) {
+        fs.unlink('/tmp/test.jpg');
+        fs.rename(files.upload.path, '/tmp/test.jpg');
+      }
+    });
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('recieved image </br>');
+    res.write('<img src="/show" />');
+    res.end();
+  });
 }
+
 function show(res) {
   console.log('request handler show was called');
   res.writeHead(200, {'Content-Type': 'image/jpg'});
