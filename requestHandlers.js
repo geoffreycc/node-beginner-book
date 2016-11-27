90040c77fdbee93d83f91354ec7d210e854e424d
Geoffrey Cox <gcox3d@gmail.com>
2016-11-26 20:48:11 -0800

    created request handler functions. Both just console.log right now.

---
+++ requestHandlers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function start() {
+  console.log('request handler start was called');
+}
+
+function upload() {
+  console.log('request handler upload was called');
+}
+
+exports.start = start;
+exports.upload = upload;
